ff2433c4ff446d87a4801942b1e94bdb818f7208
daria <bdrdria@yandex.ru>
2026-03-12 00:59:52 +0500

    add array get function

--- containers/dynamic_array.c
+++ containers/dynamic_array.c
@@ -68,3 +68,8 @@ void array_insert(DynamicArray* arr, void* elem, size_t index) {
     arr->data[index] = elem;
     arr->size++;
 }
+
+const void* array_get(const DynamicArray* arr, size_t index) {
+    if (arr == NULL || index > arr->size) return;
+    return arr->data[index];
+}
--- containers/dynamic_array.h
+++ containers/dynamic_array.h
@@ -14,3 +14,4 @@ void array_remove_by_index(DynamicArray* arr, size_t index);
 void array_destroy(DynamicArray* arr);
 void array_push_back(DynamicArray* arr, void* elem);
 void array_insert(DynamicArray* arr, void* elem, size_t index);
+const void* array_get(const DynamicArray* arr, size_t index);
