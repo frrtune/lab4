329f6b25301cb007738444e478e409ded12e0a25
daria <bdrdria@yandex.ru>
2026-03-15 21:26:11 +0500

    feat: add test insert function

--- tests/tests_dynamic_array.c
+++ tests/tests_dynamic_array.c
@@ -45,9 +45,29 @@ void test_array_remove_by_index() {
     puts("SUCCESS");
 }
 
+void test_array_insert() {
+    DynamicArray* arr = array_initialize(3, GetIntFieldInfo());
+    int* elem1 = malloc(sizeof(int));
+    int* elem2 = malloc(sizeof(int));
+    int* elem3 = malloc(sizeof(int));
+    int* elem4 = malloc(sizeof(int));
+    *elem1 = 1, *elem2 = 2, *elem3 = 3, *elem4 = 4;
+    array_push_back(arr, elem1);
+    array_push_back(arr, elem2);
+    array_push_back(arr, elem3);
+    array_insert(arr, elem4, 1);
+    assert(*(int*)arr->data[0] == 1);
+    assert(*(int*)arr->data[1] == 4);
+    assert(*(int*)arr->data[2] == 2);
+    assert(*(int*)arr->data[3] == 3);
+    array_destroy(arr);
+    puts("SUCCESS");
+}
+
 int main() {
     test_array_initialize();
     test_array_push_back();
     test_array_remove_by_index();
+    test_array_insert();
     return 0;
 }
