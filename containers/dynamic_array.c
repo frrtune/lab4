4c087c9da6b2cba35460eb3fbf00dc25eed29566
daria <bdrdria@yandex.ru>
2026-03-16 12:59:33 +0500

    feat: add error handling

--- containers/dynamic_array.c
+++ containers/dynamic_array.c
@@ -1,14 +1,51 @@
 #include "dynamic_array.h"
+#include <errno.h>
+#include <stdio.h>
+
+void show_errno(void)
+{
+    const char *err_info = "unknown error";
+    switch (errno)
+    {
+        case EDOM:
+            err_info = "domain error";
+            break;
+        case EILSEQ:
+            err_info = "illegal sequence";
+            break;
+        case ERANGE:
+            err_info = "pole or range error";
+            break;
+        case EINVAL:
+            err_info = "invalid argument";
+            break;
+        case ENOMEM:
+            err_info = "memory error";
+            break;
+        case 0:
+            err_info = "no error";
+    }
+    fputs(err_info, stdout);
+    puts(" occurred");
+}
 
 DynamicArray* array_initialize(size_t capacity, const FieldInfo* info) {
     if (!capacity || !info ) {
+        errno = EINVAL;
+        show_errno();
         return NULL;
     }
     DynamicArray* arr = (DynamicArray*)malloc(sizeof(DynamicArray));
-    if (arr == NULL) return NULL;
+    if (arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return NULL;
+    }
     arr->data = (void**)malloc(sizeof(void*) * capacity);
     if (arr->data == NULL) {
         free(arr);
+        errno = ENOMEM;
+        show_errno();
         return NULL;
     }
     arr->size = 0;
@@ -18,10 +55,15 @@ DynamicArray* array_initialize(size_t capacity, const FieldInfo* info) {
 }
 
 void array_remove_by_index(DynamicArray* arr, size_t index) {
-    if (arr == NULL) return;
+    if (arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return;
+    }
     if (index >= arr->size)
     {
-        //дописать вывод ошибки
+        errno = ERANGE;
+        show_errno();
         return;
     }
     arr->info->destroy(arr->data[index]);
@@ -34,7 +76,11 @@ void array_remove_by_index(DynamicArray* arr, size_t index) {
 }
 
 void array_destroy(DynamicArray* arr) {
-    if (arr == NULL) return;
+    if (arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return;
+    }
     while (arr->size > 0) {
         array_remove_by_index(arr, arr->size-1);
     }
@@ -43,10 +89,18 @@ void array_destroy(DynamicArray* arr) {
 }
 
 void array_push_back(DynamicArray* arr, void* elem) {
-    if (arr == NULL || elem == NULL) return;
+    if (arr == NULL || elem == NULL) {
+        errno = EINVAL;
+        show_errno();
+        return;
+    }
     if (arr->size == arr->capacity) {
         void** new_data = realloc(arr->data, 2 * arr->capacity * sizeof(void*));
-        if (new_data == NULL) return;
+        if (new_data == NULL) {
+            errno = ENOMEM;
+            show_errno();
+            return;
+        }
         arr->data = new_data;
         arr->capacity = 2 * arr->capacity;
     }       
@@ -55,10 +109,18 @@ void array_push_back(DynamicArray* arr, void* elem) {
 }
 
 void array_insert(DynamicArray* arr, void* elem, size_t index) {
-    if (arr == NULL || elem == NULL || index > arr->size) return;
+    if (arr == NULL || elem == NULL || index > arr->size) {
+        errno = EINVAL;
+        show_errno();
+        return;
+    }
     if (arr->size == arr->capacity) {
         void** new_data = realloc(arr->data, 2 * arr->capacity * sizeof(void*));
-        if (new_data == NULL) return;
+        if (new_data == NULL) {
+            errno = ENOMEM;
+            show_errno();
+            return;
+        }
         arr->data = new_data;
         arr->capacity = 2 * arr->capacity;
     }
@@ -70,17 +132,31 @@ void array_insert(DynamicArray* arr, void* elem, size_t index) {
 }
 
 const void* array_get(const DynamicArray* arr, size_t index) {
-    if (arr == NULL || index >= arr->size) return NULL;
+    if (arr == NULL || index >= arr->size) {
+        errno = EINVAL;
+        show_errno();
+        return NULL;
+    }
     return arr->data[index];
 }
 
 DynamicArray* array_map(const DynamicArray* arr, void* (*function)(const void*)) {
-    if (arr == NULL || function == NULL) return NULL;
+    if (arr == NULL || function == NULL) {
+        errno = EINVAL;
+        show_errno();
+        return NULL;
+    }
     DynamicArray* new_arr = array_initialize(arr->size, arr->info);
-    if (new_arr == NULL) return NULL;
+    if (new_arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return NULL;
+    }
     for (size_t i = 0; i < arr->size; i++) {
         void* new_elem = function(arr->data[i]);
         if (new_elem == NULL) {
+            errno = ENOMEM;
+            show_errno();
             array_destroy(new_arr);
             return NULL;
         }
@@ -90,9 +166,17 @@ DynamicArray* array_map(const DynamicArray* arr, void* (*function)(const void*))
 }
 
 DynamicArray* array_where(const DynamicArray* arr, int (*function)(const void*)) {
-    if (arr == NULL || function == NULL) return NULL;
+    if (arr == NULL || function == NULL) {
+        errno = EINVAL;
+        show_errno();
+        return NULL;
+    }
     DynamicArray* new_arr = array_initialize(arr->size, arr->info);
-    if (new_arr == NULL) return NULL;
+    if (new_arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return NULL;
+    }
     for (size_t i = 0; i < arr->size; i++) {
         if (function(arr->data[i])) {
             void* copied_elem = arr->info->clone(arr->data[i]);
@@ -103,9 +187,17 @@ DynamicArray* array_where(const DynamicArray* arr, int (*function)(const void*))
 }
 
 DynamicArray* array_concatenate(DynamicArray* arr1, DynamicArray* arr2) {
-    if (arr1 == NULL || arr2 == NULL || arr1->info != arr2->info) return NULL;
+    if (arr1 == NULL || arr2 == NULL || arr1->info != arr2->info) {
+        errno = EINVAL;
+        show_errno();
+        return NULL;
+    }
     DynamicArray* new_arr = array_initialize(arr1->size + arr2->size, arr1->info);
-    if (new_arr == NULL) return NULL;
+    if (new_arr == NULL) {
+        errno = ENOMEM;
+        show_errno();
+        return NULL;
+    }
     for (size_t i = 0; i < arr1->size; i++) {
         void* copied_elem = arr1->info->clone(arr1->data[i]);
         array_push_back(new_arr, copied_elem);
