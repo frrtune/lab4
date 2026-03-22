c9c639f92c8704c2d9f83c725bfb7dacfe157300
daria <bdrdria@yandex.ru>
2026-03-22 23:54:39 +0500

    feat: add complex menu to ui

--- ui/ui.c
+++ ui/ui.c
@@ -61,6 +61,77 @@ void integer_menu() {
     array_destroy(arr);
 }
 
+void complex_menu() {
+    DynamicArray* arr = array_initialize(5, GetComplexFieldInfo());
+    int in_progress = 1;
+    char re_buff[15];
+    char im_buff[15];
+    while (in_progress) {
+        clear();
+        printw("COMPLEX NUMBERS\n");
+        printw("Current array: ");
+        if (arr->size == 0) {
+            printw("[]");
+        } else {
+            printw("[");
+            for (size_t i = 0; i < arr->size; i++) {
+                Complex* current = (Complex*)((char*)arr->data + (arr->info->size * i));
+                printw("%d", current->Re);
+                if (current->Im >= 0) {
+                    printw("+ %di", current -> Im);
+                } else {
+                    printw("%di", current -> Im);
+                }
+                if (i < arr->size - 1) printw(", ");
+            }
+            printw("]");
+        }
+        printw("\n1. Add element\n");
+        printw("2. Remove last\n");
+        printw("3. Back\n");
+        printw("Choose:\n");
+        refresh();
+        int ch = getch();
+        switch(ch) {
+            case '1':
+                echo();
+                curs_set(1);
+                printw("\nEnter the real part of a number: ");
+                refresh();
+                scanw("%s", re_buff);
+                printw("\nEnter the imaginary part of a number: ");
+                refresh();
+                scanw("%s", im_buff);
+                noecho();
+                curs_set(0);
+                char* endptr1;
+                char* endptr2;
+                errno = 0;
+                long re_value = strtol(re_buff, &endptr1, 10);
+                long im_value = strtol(re_buff, &endptr2, 10);
+                if (errno == 0 && endptr1 == "\0" && endptr2 == "\0"){
+                    Complex* new_elem = malloc(sizeof(Complex));
+                    new_elem->Re = re_value;
+                    new_elem->Im = im_value;
+                    array_push_back(arr, new_elem);
+                } else {
+                    printw("Error: invalud input");
+                    refresh();
+                    getch();
+                }
+                break;
+            case '2':
+                if (arr->size > 0) array_remove_by_index(arr, arr->size - 1);
+                getch();
+                break;  
+            case '3':
+                in_progress = 0;
+                break;
+        }
+    }
+    array_destroy(arr);
+}
+
 void open_ui() {
     initscr();
     noecho();
@@ -79,10 +150,7 @@ void open_ui() {
                 break;
             case '2':
                 clear();
-                printw("Complex numbers\n");
-                printw("Press any key to exit");
-                refresh();
-                getch();
+                complex_menu();
                 break;
             case '3':
                 in_progress = 0;
